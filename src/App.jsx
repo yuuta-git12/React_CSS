8611653f670c32953d4adcde21cb6fb643743b3f
yuuta-git12 <git@stackblitz.com>
2024-11-29 22:10:51 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { InlineStyle } from './components/InlineStyle';
+
+function App() {
+  return (
+    <div className="App">
+      <InlineStyle />
+    </div>
+  );
+}
+
+export default App;
