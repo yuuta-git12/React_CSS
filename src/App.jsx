4dd905ed0728d5f7fec898ed6269d523b6d2516f
yuuta-git12 <tayu2201@icloud.com>
2024-11-29 22:59:50 +0900

    CSS Modulesのサンプルコード追加

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 // import './App.css';
 import { InlineStyle } from './components/InlineStyle';
+import { CssModules } from './components/ CssModules';
 
 function App() {
   return (
     <div className="App">
       <InlineStyle />
+      <CssModules />
     </div>
   );
 }
