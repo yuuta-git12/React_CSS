87be2cee007cbf617194fe3bdc15fdd12f506739
yuuta-git12 <tayu2201@icloud.com>
2024-11-30 12:14:47 +0900

    styled-jsxのサンプルコード追加

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import viteLogo from '/vite.svg';
 // import './App.css';
 import { InlineStyle } from './components/InlineStyle';
 import { CssModules } from './components/ CssModules';
+import { StyledJsx } from './components/StyledJsx';
 
 function App() {
   return (
     <div className="App">
       <InlineStyle />
       <CssModules />
+      <StyledJsx />
     </div>
   );
 }
